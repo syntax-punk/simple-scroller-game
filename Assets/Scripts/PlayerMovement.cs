15c831b30e4af84cfd182fa543fe99984d2659bd
syntax-punk <david.jaeren@gmail.com>
2024-12-09 10:51:44 +0100

    Connected movement to the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,35 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    float runSpeed = 6f;
+
+    [SerializeField]
+    float runBoost = 20f;
+
+    [SerializeField]
+    float gravity = 1f;
+
+
     private Vector2 _moveInput;
+    private Rigidbody2D _rb;
 
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        Run();
+    }
 
+    private void Run()
+    {
+        var currentVelocity = new Vector2(_moveInput.x * runSpeed, _rb.linearVelocityY);
+        _rb.linearVelocity = currentVelocity;
     }
 
     void OnMove(InputValue value)
