23e10dcb64743b1748fdccb2cd0f171eb541a34a
syntax-punk <david.jaeren@gmail.com>
2024-12-09 12:11:50 +0100

    Movement animation added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,13 +14,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float gravity = 1f;
 
-
     private Vector2 _moveInput;
     private Rigidbody2D _rb;
+    private Animator _animator;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -30,11 +33,26 @@ public class PlayerMovement : MonoBehaviour
     {
         var currentVelocity = new Vector2(_moveInput.x * runSpeed, _rb.linearVelocityY);
         _rb.linearVelocity = currentVelocity;
+
+        var playerIsMovingHorizontally = Mathf.Abs(_moveInput.x) > Mathf.Epsilon;
+
+        if (playerIsMovingHorizontally)
+        {
+            FlipSprite();
+        }
+
+        _animator.SetBool("isRunning", playerIsMovingHorizontally);
+    }
+
+    private void FlipSprite()
+    {
+        var signX = Mathf.Sign(_moveInput.x);
+        var currentScale = new Vector3(signX, transform.localScale.y, transform.localScale.z);
+        transform.localScale = currentScale;
     }
 
     void OnMove(InputValue value)
     {
         _moveInput = value.Get<Vector2>();
-        Debug.Log(_moveInput);
     }
 }
